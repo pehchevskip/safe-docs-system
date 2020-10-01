aa5f4318e78ad43650185131d9b6706015f29812
pehchevskip <petar.pehchevski@gmail.com>
2020-10-02 00:06:53 +0200

    Update api url

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://tryingtocrypt-env-1.eba-mb8fzgip.eu-central-1.elasticbeanstalk.com/api'
+  apiUrl: 'http://safedocssystem-env.eba-3jktnzhp.eu-central-1.elasticbeanstalk.com/api'
 };
